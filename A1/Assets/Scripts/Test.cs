8a36d827210c0938006e7b096e93fe58d4e8542f
Ricardo Liganor <ricardo.liganor@hotmail.com>
2019-01-29 23:33:00 -0500

    final

--- A1/Assets/Scripts/Test.cs
+++ A1/Assets/Scripts/Test.cs
@@ -4,19 +4,279 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public Vector2 p1 = new Vector2(8, 2);
+    public  Vector2 p2 = new Vector2(5, 6);
+    public float Vm = 7;
+    public float am = 10;
+    public Vector2 pc = new Vector2(8.2f, 2.3f);
+    public Vector2 vc = new Vector2(0, 0);
+    public float t = 0.5f;
+    public float velv;
+    public float ra = 0.5f;
+    public float rs = 6.0f;
+    public float t2t = 1.0f;
+
+
+    
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 vd = p1 - p2;
+        Debug.Log("vd = " + vd);
+        Vector2 Nvd = vd.normalized;
+        Debug.Log("Nvd = " +  Nvd);
+        velv = vd.magnitude;
+
+        Vector2 a;
+        Vector2 v;
+
+        if(velv < rs)
+        {
+            Vector2 govel = ((velv / rs) * Vm) * Nvd;
+            a = (govel - vc) / t2t;
+
+            if(a.magnitude >= am)
+            {
+                a = a.normalized * Vm;
+            }
+
+            if(velv < ra)
+            {
+                v = new Vector2(0,0);
+            }
+            else
+            {
+                v = vc + (a * t);
+            }
+
+            if(v.magnitude >= Vm)
+            {
+                v = v.normalized * Vm;
+            }
+
+            pc = pc + (v * t);
+
+            Debug.Log("velv = " + velv);
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+        else
+        {
+            a = am * Nvd;
+            v = vc + a * t;
+
+            if(v.magnitude >= Vm)
+            {
+                v = v.normalized * Vm;
+            }
+
+            pc = pc + (v * t);
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+
+        Debug.Log("Point 3");
+        if (velv < rs)
+        {
+            Vector2 govel = ((velv / rs) * Vm) * (Nvd);
+            a = (govel - vc) / t2t;
+
+            if (a.magnitude >= am)
+            {
+                a = a.normalized * Vm;
+            }
+
+            if (velv < ra)
+            {
+                v = new Vector2(0, 0);
+            }
+            else
+            {
+                v = vc + (a * t);
+            }
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+        else
+        {
+            a = am * Nvd;
+            v = vc + a * t;
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+
+        Debug.Log("Point 2");
+        if (velv < rs)
+        {
+            Vector2 govel = ((velv / rs) * Vm) * (Nvd);
+            a = (govel - vc) / t2t;
+
+            if (a.magnitude >= am)
+            {
+                a = a.normalized * Vm;
+            }
+
+            if (velv < ra)
+            {
+                v = new Vector2(0, 0);
+            }
+            else
+            {
+                v = vc + (a * t);
+            }
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+        else
+        {
+            a = am * Nvd;
+            v = vc + a * t;
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+        Debug.Log("Point 4");
+        if (velv < rs)
+        {
+            Vector2 govel = ((velv / rs) * Vm) * (Nvd);
+            a = (govel - vc) / t2t;
+
+            if (a.magnitude >= am)
+            {
+                a = a.normalized * Vm;
+            }
+
+            if (velv < ra)
+            {
+                v = new Vector2(0, 0);
+            }
+            else
+            {
+                v = vc + (a * t);
+            }
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+        else
+        {
+            a = am * Nvd;
+            v = vc + a * t;
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+        Debug.Log("Point 5");
+        if (velv < rs)
+        {
+            Vector2 govel = ((velv / rs) * Vm) * (Nvd);
+            a = (govel - vc) / t2t;
+
+            if (a.magnitude >= am)
+            {
+                a = a.normalized * Vm;
+            }
+
+            if (velv < ra)
+            {
+                v = new Vector2(0, 0);
+            }
+            else
+            {
+                v = vc + (a * t);
+            }
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+        else
+        {
+            a = am * Nvd;
+            v = vc + a * t;
+
+            if (v.magnitude >= Vm)
+            {
+                v = (v.normalized) * Vm;
+            }
+
+            pc = pc + (v * t);
+            Debug.Log("a = " + a);
+            Debug.Log("v = " + v);
+            Debug.Log("pc = " + pc);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.5)
+        //if (gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.5)
         {
 
-            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * .5f);
+            //gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * .5f);
         }
     }
 }
---
+++ A1/README.txt
@@ -0,0 +1,10 @@
+Ricardo Liganor
+26994377
+
+Implementation of code for assginment 1
+
+NPC actions are controlled by the NPC script, 
+Flag Script controls the movement back and forth of the flag, 
+Gamecontroller controls the state of the game. 
+
+This does still have bugs, and I appoligize for that. 
